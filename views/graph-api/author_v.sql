57934f8e7efa74ff3fdcf4f7a6e94e37c1e3a8a4
Greg Whitbread <whitbread.greg@gmail.com>
2023-01-10 14:18:32 +1100

    NSL-4381: A new view, AUTHOR_V, to deliver a standard author object.

---
+++ views/graph-api/author_v.sql
@@ -0,0 +1,28 @@
+-- author_v.sql
+-- A standard author view
+-- supports:  reference_v
+--            name_v
+
+drop view if exists qview.AUTHOR_V;
+create view qview.AUTHOR_V as
+--   columns
+--   (
+--     author_id, identifier, standard_form, see_also,
+--     author_name, author_full_name, nsl_shard
+--     )
+SELECT *
+FROM (SELECT a.id                                               as author_id
+           , host.value || 'author/' || p.rdf_id || '/' || a.id as identifier
+	         --  , orcid ?
+           , abbrev                                             as standard_Form
+	       -- [todo] should not be ipni_Id, /* move to generic term for URI for IPNI, ZooBank, Index Fungorum,... */
+           , ipni_id                                            as see_also  -- a name for this ? other_id
+           , a.name                                             as author_name
+           , full_name                                          as author_full_Name
+           , p.rdf_id                                           as nsl_shard /* will be URI */
+      from author a
+	           join namespace p
+	                on a.namespace_id = p.id
+	           join shard_config host on host.name = 'mapper host'
+      where duplicate_of_id is null) auth_v
+;
